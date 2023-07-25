70ce18ff6d831a4577161926047901bc62a5a6f9
ふーしゃ / Fuchsia <46129202+fuchsia-84@users.noreply.github.com>
2023-07-25 17:13:11 +0900

    add faq contents and accordion js.

---
+++ js/accordion.js
@@ -0,0 +1,15 @@
+var title = $('.accordion h1');
+
+//アコーディオンをクリックした時の動作
+title.on('click', function() {//タイトル要素をクリックしたら
+    console.log('clocked title.');
+    var findElm = $(this).next(".box");//直後のアコーディオンを行うエリアを取得し
+    $(findElm).slideToggle();//アコーディオンの上下動作
+      
+    if($(this).hasClass('close')){//タイトル要素にクラス名closeがあれば
+      $(this).removeClass('close');//クラス名を除去し
+    }else{//それ以外は
+      $(this).addClass('close');//クラス名closeを付与
+    }
+  });
+  

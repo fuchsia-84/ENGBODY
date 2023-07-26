e975c82b42257a406c940492a8a17f2399eb2d9c
ふーしゃ / Fuchsia <46129202+fuchsia-84@users.noreply.github.com>
2023-07-26 15:43:34 +0900

    fix header fixed position.

--- js/header_fixed.js
+++ js/header_fixed.js
@@ -1,6 +1,6 @@
 $(function() {
     var $header = $('#header');
-    var change_header_position = $("#top").height() - $("#header").outerHeight(true);
+    var change_header_position = $("#header").outerHeight(true);
     // Header Fixed
     $(window).scroll(function() {
         if ($(window).scrollTop() > change_header_position) { 

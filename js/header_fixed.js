f302eca5867ca46fc58d78673678c3b77742764b
ふーしゃ / Fuchsia <46129202+fuchsia-84@users.noreply.github.com>
2023-08-01 09:38:52 +0900

    add page top button.

--- js/header_fixed.js
+++ js/header_fixed.js
@@ -1,6 +1,9 @@
 $(function() {
     var $header = $('#header');
+    var $page_top_btn = $('#page_top_btn');
     var change_header_position = $("#header").outerHeight(true);
+    var change_page_top_btn_position = 800;
+
     // Header Fixed
     $(window).scroll(function() {
         if ($(window).scrollTop() > change_header_position) { 
@@ -9,4 +12,15 @@ $(function() {
             $header.removeClass('fixed');
         }
     });
+
+    // Page Top Button Fixed
+    $(window).scroll(function() {
+        if ($(window).scrollTop() > change_page_top_btn_position) {
+            $page_top_btn.addClass('fixed');
+        } else {
+            $page_top_btn.removeClass('fixed');
+        }
+
+    });
+
 });

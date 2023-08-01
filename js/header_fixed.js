53c11db458c7481e89ed2ace5ebd4b44fd7a2963
ふーしゃ / Fuchsia <46129202+fuchsia-84@users.noreply.github.com>
2023-08-01 10:16:11 +0900

    add price contents scroll right.

--- js/header_fixed.js
+++ js/header_fixed.js
@@ -1,6 +1,8 @@
 $(function() {
     var $header = $('#header');
     var $page_top_btn = $('#page_top_btn');
+    var $price_contents = $('.price_contents');
+    var $scroll_right = $('.scroll_right');
     var change_header_position = $("#header").outerHeight(true);
     var change_page_top_btn_position = 800;
 
@@ -21,6 +23,15 @@ $(function() {
             $page_top_btn.removeClass('fixed');
         }
 
+    });
+
+    // Price Scroll Right
+    $price_contents.scroll(function() {
+        if ($price_contents.scrollLeft()) { 
+            $scroll_right.addClass('fixed');
+        } else {
+            $scroll_right.removeClass('fixed');
+        }
     });
 
 });
